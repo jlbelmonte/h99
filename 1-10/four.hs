e09e7a001544f9b46cbad194fa19e0726ca30f40
juan luis belmonte <jlbelmonte@gmail.com>
2012-12-30 18:57:55 -0800

    fourth exercise with function composition

--- 1-10/four.hs
+++ 1-10/four.hs
@@ -7,3 +7,7 @@ myLength' [] = 0;
 myLength' xs = 1 + (myLength $ tail xs)
 
 myLength'' xs = foldl (\acc x -> acc+1) 0 xs
+
+myLength''' = sum . map (\_ -> 1)
+
+myLength'''' xs = sum $ map (\x -> 1) xs
